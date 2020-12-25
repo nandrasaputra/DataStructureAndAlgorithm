d908dd55ea965f8407c4cb6d48fe27eeafbfb2ee
Nandra Saputra <nandra.saputras@gmail.com>
2020-12-26 00:49:25 +0700

    update MissingNumber leetcode problem

--- src/problems/leetcode/MissingNumber.kt
+++ src/problems/leetcode/MissingNumber.kt
@@ -3,7 +3,17 @@ package problems.leetcode
 object MissingNumber {
     class Solution {
         fun missingNumber(nums: IntArray): Int {
-            return 0
+            val numSize = nums.size
+            var actualSumNumber = 0
+            var expectedSumNumber = 0
+            for (number in 0..numSize) {
+                expectedSumNumber += number
+            }
+            nums.forEach {
+                actualSumNumber += it
+            }
+            val missingNumber = expectedSumNumber - actualSumNumber
+            return missingNumber
         }
     }
 }
