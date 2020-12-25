cc762671e31e08f7fd2bbb912cd50f8d4b8b7992
Nandra Saputra <nandra.saputras@gmail.com>
2020-12-26 00:51:16 +0700

    add MissingNumber example

--- src/problems/leetcode/MissingNumber.kt
+++ src/problems/leetcode/MissingNumber.kt
@@ -4,6 +4,11 @@ package problems.leetcode
 *       LeetCode Problem : MissingNumber
 *       https://leetcode.com/problems/missing-number/
 *
+*       example:
+*       Input: nums = [3,0,1]
+*       Output: 2
+*       Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3].
+*       2 is the missing number in the range since it does not appear in nums.
 * */
 
 object MissingNumber {
