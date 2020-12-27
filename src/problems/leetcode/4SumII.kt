3583a5319d967241d6c93c279ab234d77f5ba85d
Nandra Saputra <nandra.saputras@gmail.com>
2020-12-27 19:46:32 +0700

    complete 4SumII Problem with solution

--- src/problems/leetcode/4SumII.kt
+++ src/problems/leetcode/4SumII.kt
@@ -1,31 +1,56 @@
 package problems.leetcode
 
+/*
+*       LeetCode Problem : 4SumII
+*       https://leetcode.com/problems/4sum-ii/
+* */
+
 object SumII {
     class Solution {
         fun fourSumCount(A: IntArray, B: IntArray, C: IntArray, D: IntArray): Int {
-            return 1
-        }
-    }
+            val x = mutableMapOf<Int, Int>()
+            for (aElement in A) {
+                for (bElement in B) {
+                    val count = x[aElement + bElement]
+                    if (count != null) {
+                        x[aElement + bElement] = count + 1
+                    } else {
+                        x[aElement + bElement] = 1
+                    }
+                }
+            }
 
-    class SumCount2Solver(A: IntArray, B: IntArray, C: IntArray, D: IntArray) {
-        val n = A.size-1
-        var result = 0
+            val y = mutableMapOf<Int, Int>()
+            for (cElement in C) {
+                for (dElement in D) {
+                    val count = y[cElement + dElement]
+                    if (count != null) {
+                        y[cElement + dElement] = count + 1
+                    } else {
+                        y[cElement + dElement] = 1
+                    }
+                }
+            }
 
-        fun getEligibleTupleCount() : Int {
-            searchTuple(0, 0)
+            var result = 0
+
+            for (key in x.keys) {
+                val element = y[-key]
+                if (element != null) {
+                    result += x[key]!! * y[-key]!!
+                }
+            }
+
             return result
         }
-
-        fun searchTuple(currentPosition: Int, currentSum: Int) {
-            if (currentPosition > 3) {
-                if (currentSum == 0) {
-                    result++
-                } else {
-                    return
-                }
-            } else {
-
-            }
-        }
     }
+}
+
+fun main() {
+    print(SumII.Solution().fourSumCount(
+        intArrayOf(1,2),
+        intArrayOf(-2,-1),
+        intArrayOf(-1,2),
+        intArrayOf(0,2)
+    ))
 }
