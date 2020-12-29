8695510483ab1edf4cb3be125e0f1d636755441c
Nandra Saputra <nandra.saputras@gmail.com>
2020-12-30 06:13:45 +0700

    add MakingAnagram hackerrank problem

---
+++ src/problems/hackerrank/MakingAnagram.kt
@@ -0,0 +1,20 @@
+package problems.hackerrank
+
+import java.util.*
+
+fun makeAnagram(a: String, b: String): Int {
+
+    return 0
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val a = scan.nextLine()
+
+    val b = scan.nextLine()
+
+    val res = makeAnagram(a, b)
+
+    println(res)
+}
