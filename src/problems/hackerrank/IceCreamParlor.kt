d2bec7996364c311266df5c864fbd093683cccea
Nandra Saputra <nandra.saputras@gmail.com>
2021-01-02 09:31:55 +0700

    add IceCreamParlor HackerRank Problem

---
+++ src/problems/hackerrank/IceCreamParlor.kt
@@ -0,0 +1,23 @@
+package problems.hackerrank
+
+import java.util.*
+
+fun whatFlavors(cost: Array<Int>, money: Int): Unit {
+
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val t = scan.nextLine().trim().toInt()
+
+    for (tItr in 1..t) {
+        val money = scan.nextLine().trim().toInt()
+
+        val n = scan.nextLine().trim().toInt()
+
+        val cost = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
+
+        whatFlavors(cost, money)
+    }
+}
