04b9f375efecb2297244d27b32f7c3afd404a64d
Nandra Saputra <nandra.saputras@gmail.com>
2021-01-04 06:27:21 +0700

    add DayOfProgrammer HackerRank Problem

---
+++ src/problems/hackerrank/DayOfProgrammer.kt
@@ -0,0 +1,14 @@
+package problems.hackerrank
+
+// Complete the dayOfProgrammer function below.
+fun dayOfProgrammer(year: Int): String {
+
+}
+
+fun main(args: Array<String>) {
+    val year = readLine()!!.trim().toInt()
+
+    val result = dayOfProgrammer(year)
+
+    println(result)
+}
