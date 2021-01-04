a0b0acff9774f1c171ef49d382d148949373e0e8
Nandra Saputra <nandra.saputras@gmail.com>
2021-01-04 07:16:48 +0700

    add DayOfProgrammer HackerRank Problem Website

--- src/problems/hackerrank/DayOfProgrammer.kt
+++ src/problems/hackerrank/DayOfProgrammer.kt
@@ -1,5 +1,10 @@
 package problems.hackerrank
 
+/*
+        HackerRank: DayOfProgrammer
+*       https://www.hackerrank.com/challenges/day-of-the-programmer/problem
+* */
+
 // Complete the dayOfProgrammer function below.
 fun dayOfProgrammer(year: Int): String {
 
