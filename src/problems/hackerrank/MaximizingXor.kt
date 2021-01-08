de9bf520a63424a21cde3db6221e8c4028944e3d
Nandra Saputra <nandra.saputras@gmail.com>
2021-01-08 11:14:43 +0700

    add hackerrank maximizing xor website and solution

--- src/problems/hackerrank/MaximizingXor.kt
+++ src/problems/hackerrank/MaximizingXor.kt
@@ -1,8 +1,14 @@
 package problems.hackerrank
 
+import java.util.*
+/*
+*       HackerRank: MaximizingXor
+*       https://www.hackerrank.com/challenges/maximizing-xor/problem
+*/
 fun maximizingXor(l: Int, r: Int): Int {
-
-
+    val xored = l xor r
+    val significantBit = 31 - Integer.numberOfLeadingZeros(xored)
+    return (1 shl significantBit + 1) - 1
 }
 
 fun main(args: Array<String>) {
