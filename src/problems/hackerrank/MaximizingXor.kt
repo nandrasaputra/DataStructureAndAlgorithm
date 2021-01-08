b824177fef87e7ce42c6918d66de163e3b635d02
Nandra Saputra <nandra.saputras@gmail.com>
2021-01-08 10:32:58 +0700

    add hackerrank maximizing xor problem

---
+++ src/problems/hackerrank/MaximizingXor.kt
@@ -0,0 +1,18 @@
+package problems.hackerrank
+
+fun maximizingXor(l: Int, r: Int): Int {
+
+
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val l = scan.nextLine().trim().toInt()
+
+    val r = scan.nextLine().trim().toInt()
+
+    val result = maximizingXor(l, r)
+
+    println(result)
+}
