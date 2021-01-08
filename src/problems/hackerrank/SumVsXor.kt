54a1809c4890983771e03ae29d2b02ad86a7be61
Nandra Saputra <nandra.saputras@gmail.com>
2021-01-09 06:56:00 +0700

    add hackerrank sum vs xor solution

--- src/problems/hackerrank/SumVsXor.kt
+++ src/problems/hackerrank/SumVsXor.kt
@@ -1,8 +1,19 @@
 package problems.hackerrank
 
 fun sumXor(n: Long): Long {
-
-
+    if (n == (0.toLong())) {
+        return 1
+    } else {
+        val twoBitRepresentation = n.toString(2).toCharArray()
+        var number = 0
+        val base = 2
+        for (index in twoBitRepresentation.indices) {
+            if (twoBitRepresentation[index] == '0') {
+                number++
+            }
+        }
+        return Math.pow(base.toDouble(),number.toDouble()).toLong()
+    }
 }
 
 fun main(args: Array<String>) {
