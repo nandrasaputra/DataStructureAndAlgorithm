bf4a2d8b6fba7b4e842739a5fea6995ada2d01bc
Nandra Saputra <nandra.saputras@gmail.com>
2021-01-10 10:11:42 +0700

    add hackerrank Angry Professor website

--- src/problems/hackerrank/AngryProfessor.kt
+++ src/problems/hackerrank/AngryProfessor.kt
@@ -1,5 +1,10 @@
 package problems.hackerrank
 
+/*
+*   HackerRank: Angry Professor
+*   https://www.hackerrank.com/challenges/angry-professor/problem
+*/
+
 fun angryProfessor(k: Int, a: Array<Int>): String {
     var studentPresent = 0
     for (element in a) {
