4d450ee39d0f38ee15c427a6ab0863e91f427ae8
Nandra Saputra <nandra.saputras@gmail.com>
2021-01-17 10:58:32 +0700

    add hackerrank Cat and Mouse Website

--- src/problems/hackerrank/CatAndMouse.kt
+++ src/problems/hackerrank/CatAndMouse.kt
@@ -1,5 +1,10 @@
 package problems.hackerrank
 
+/*
+*   HackerRank: Cat and Mouse
+*   https://www.hackerrank.com/challenges/cats-and-a-mouse/problem
+*/
+
 fun catAndMouse(x: Int, y: Int, z: Int): String {
     val catADistance = Math.abs(x-z)
     val catBDistance = Math.abs((y-z))
