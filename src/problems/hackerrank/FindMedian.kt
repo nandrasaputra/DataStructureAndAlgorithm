54c9ea7838ccaed45393817a079504daa08c98a7
Nandra Saputra <nandra.saputras@gmail.com>
2021-01-17 09:40:09 +0700

    add hackerrank Find Median Website

--- src/problems/hackerrank/FindMedian.kt
+++ src/problems/hackerrank/FindMedian.kt
@@ -1,5 +1,10 @@
 package problems.hackerrank
 
+/*
+*   HackerRank: Find Median
+*   https://www.hackerrank.com/challenges/find-the-median/problem
+*/
+
 fun findMedian(arr: Array<Int>): Int {
     arr.sort()
     val targetIndex = ((arr.size - 2) / 2) + 1
