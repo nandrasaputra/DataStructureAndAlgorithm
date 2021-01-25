d56e38df0f3049b592b16e3956f9ee47f482f834
Nandra Saputra <nandra.saputras@gmail.com>
2021-01-25 16:29:50 +0700

    add hacker rank : Drawing Book website

--- src/problems/hackerrank/DrawingBook.kt
+++ src/problems/hackerrank/DrawingBook.kt
@@ -1,5 +1,10 @@
 package problems.hackerrank
 
+/*
+*   HackerRank: Drawing Book
+*   https://www.hackerrank.com/challenges/drawing-book/problem
+*/
+
 fun pageCount(n: Int, p: Int): Int {
     val totalPageTurnCountFromFront = n / 2
     val targetPageTurnCountFromFront = p / 2
