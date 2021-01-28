cca708863c2d04d31c047dfe456b2cf993f269ac
Nandra Saputra <nandra.saputras@gmail.com>
2021-01-28 18:37:11 +0700

    add hackerrank: Sherlocks and Square website

--- src/problems/hackerrank/SherlocksAndSquare.kt
+++ src/problems/hackerrank/SherlocksAndSquare.kt
@@ -1,5 +1,9 @@
 package problems.hackerrank
 
+/*
+*   HackerRank: Sherlocks and Square
+*   https://www.hackerrank.com/challenges/sherlock-and-squares/problem
+*/
 fun squares(a: Int, b: Int): Int {
     val aRoot = Math.ceil(Math.sqrt(a.toDouble()))
     val bRoot = Math.floor(Math.sqrt(b.toDouble()))
