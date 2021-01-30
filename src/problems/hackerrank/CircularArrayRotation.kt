8089bc22d2627a1ad28e2145c141ec7d42d7cdcd
Nandra Saputra <nandra.saputras@gmail.com>
2021-01-30 19:56:43 +0700

    add hackerrank: Circular Array Rotation Website

--- src/problems/hackerrank/CircularArrayRotation.kt
+++ src/problems/hackerrank/CircularArrayRotation.kt
@@ -1,5 +1,10 @@
 package problems.hackerrank
 
+/*
+*   HackerRank: Circular Array Rotation
+*   https://www.hackerrank.com/challenges/circular-array-rotation/problem
+*/
+
 fun circularArrayRotation(a: Array<Int>, k: Int, queries: Array<Int>): Array<Int> {
     val n = a.size
     val moveCount = k % n
