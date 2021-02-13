ffa7e422ccf3c23a30492116f41982c44d41660f
Nandra Saputra <nandra.saputras@gmail.com>
2021-02-13 11:11:34 +0700

    add hackerrank: Chocolate Feast Problem

---
+++ src/problems/hackerrank/ChocolateFeast.kt
@@ -0,0 +1,20 @@
+package problems.hackerrank
+
+fun chocolateFeast(n: Int, c: Int, m: Int): Int {
+    var chocolateCount = n/c
+    var chocolateNow = chocolateCount
+    var chocolateWrapper = 0
+    while (chocolateNow > 0) {
+        chocolateWrapper += chocolateNow
+
+        val chocoGain = chocolateWrapper / m
+        val usedWrapper = chocoGain * m
+        val wrapperLeft = chocolateWrapper - usedWrapper
+
+        chocolateNow = chocoGain
+        chocolateCount += chocoGain
+        chocolateWrapper = wrapperLeft
+    }
+
+    return chocolateCount
+}
