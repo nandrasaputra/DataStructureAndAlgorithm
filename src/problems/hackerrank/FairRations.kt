a48b28cd612c2c5441f0255b9a5d2ec5ac56bef4
Nandra Saputra <nandra.saputras@gmail.com>
2021-02-15 19:55:47 +0700

    add hackerrank: Fair Rations Problem and Solution

---
+++ src/problems/hackerrank/FairRations.kt
@@ -0,0 +1,16 @@
+package problems.hackerrank
+
+fun fairRations(B: Array<Int>): Int {
+    var breadUsed = 0
+    for (index in B.indices) {
+        if (B[index] % 2 != 0) {
+            if (index + 1 < B.size) {
+                B[index + 1] += 1
+                breadUsed += 2
+            } else {
+                print("NO")
+            }
+        }
+    }
+    return breadUsed
+}
