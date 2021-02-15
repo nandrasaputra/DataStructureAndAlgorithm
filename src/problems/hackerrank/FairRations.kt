4eda346e395bd93c6599c03dca386a834a11a146
Nandra Saputra <nandra.saputras@gmail.com>
2021-02-15 19:56:18 +0700

    add hackerrank: Fair Rations Website

--- src/problems/hackerrank/FairRations.kt
+++ src/problems/hackerrank/FairRations.kt
@@ -1,5 +1,10 @@
 package problems.hackerrank
 
+/*
+*   HackerRank: Fair Rations
+*   https://www.hackerrank.com/challenges/fair-rations/problem
+*/
+
 fun fairRations(B: Array<Int>): Int {
     var breadUsed = 0
     for (index in B.indices) {
