33a68c22766a74ae0679261af8e3222650d690df
Nandra Saputra <nandra.saputras@gmail.com>
2021-02-22 09:32:11 +0700

    add hackerrank: Modified Kaprekar Number Problem and Solution

---
+++ src/problems/hackerrank/ModifiedKaprekarNumber.kt
@@ -0,0 +1,23 @@
+package problems.hackerrank
+
+fun kaprekarNumbers(p: Int, q: Int): Unit {
+    val kaprekarNumberList: MutableList<Int> = mutableListOf()
+    for (number in p..q) {
+        val numberSquareString = (number.toLong() * number).toString()
+        val left = numberSquareString.substring(0,(numberSquareString.length/2))
+        val right = numberSquareString.substring((numberSquareString.length/2))
+
+        val leftNumber = if (left.isEmpty()) 0 else left.toInt()
+        val rightNumber = if (right.isEmpty()) 0 else right.toInt()
+
+        if (leftNumber + rightNumber == number) {
+            kaprekarNumberList.add(number)
+        }
+    }
+
+    if (kaprekarNumberList.isEmpty()) {
+        println("INVALID RANGE")
+    } else {
+        println(kaprekarNumberList)
+    }
+}
