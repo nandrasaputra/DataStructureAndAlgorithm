50c903dd0cc1afc36e926e09797136bdbdc55a71
Nandra Saputra <nandra.saputras@gmail.com>
2021-02-23 13:45:16 +0700

    add hackerrank: Manasa and Stones Problem and Solution

---
+++ src/problems/hackerrank/ManasaAndStones.kt
@@ -0,0 +1,14 @@
+package problems.hackerrank
+
+fun stones(n: Int, a: Int, b: Int): Array<Int> {
+    val setOfEndStone = mutableSetOf<Int>()
+    for(i in 0 until n) {
+        val j = n - 1 - i
+
+        val stoneA = a * i
+        val stoneB = b * j
+        val result = stoneA + stoneB
+        setOfEndStone.add(result)
+    }
+    return setOfEndStone.toTypedArray().sortedArray()
+}
