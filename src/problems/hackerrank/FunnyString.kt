ecdcdcaf1e09ba9f16c285e9b79d918b3c3dcf4a
Nandra Saputra <nandra.saputras@gmail.com>
2021-02-28 10:26:41 +0700

    add hackerrank: Funny String Solution

--- src/problems/hackerrank/FunnyString.kt
+++ src/problems/hackerrank/FunnyString.kt
@@ -1,23 +1,15 @@
 package problems.hackerrank
 
-// TODO: TEMPORARY SOLUTION
 fun funnyString(s: String): String {
     val reverseString = s.reversed()
     var firstPointer = 0
-    var difference: Int? = null
     for (secondPointer in 1 until s.length) {
-        val currentDifference = Math.abs(s[firstPointer].toInt() - s[secondPointer].toInt())
-        difference?.let {
-            if (currentDifference != difference) {
-                return "Not Funny"
-            }
+        val stringDiff = Math.abs(s[firstPointer].toInt() - s[secondPointer].toInt())
+        val reversedStringDiff = Math.abs(reverseString[firstPointer].toInt() - reverseString[secondPointer].toInt())
+        if (stringDiff != reversedStringDiff) {
+            return "Not Funny"
         }
-        difference = currentDifference
         firstPointer++
     }
     return "Funny"
-}
-
-fun main() {
-    print(funnyString("acxz"))
 }
