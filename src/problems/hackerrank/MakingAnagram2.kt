59b1c2ede46a159499b5d9ede6a2a7620aef4da6
Nandra Saputra <nandra.saputras@gmail.com>
2021-03-02 09:52:24 +0700

    add hackerrank: Making Anagram Problem and Solution

---
+++ src/problems/hackerrank/MakingAnagram2.kt
@@ -0,0 +1,25 @@
+package problems.hackerrank
+
+fun makingAnagrams(s1: String, s2: String): Int {
+    val baseChar = 'a'.toInt()
+    val charCountArrayA = IntArray(26) {0}
+    val charCountArrayB = IntArray(26) {0}
+    s1.forEach {
+        val arrayIndex = it.toInt() - baseChar
+        charCountArrayA[arrayIndex]++
+    }
+
+    s2.forEach {
+        val arrayIndex = it.toInt() - baseChar
+        charCountArrayB[arrayIndex]++
+    }
+
+    var result = 0
+
+    for (index in charCountArrayA.indices) {
+        val difference = Math.abs(charCountArrayA[index] - charCountArrayB[index])
+        result += difference
+    }
+
+    return result
+}
