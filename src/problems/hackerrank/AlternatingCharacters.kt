9eeb4e98267f303e8a9036d4c3395f24d6bca041
Nandra Saputra <nandra.saputras@gmail.com>
2021-03-08 18:27:40 +0700

    add hackerrank: Alternating Character Website

--- src/problems/hackerrank/AlternatingCharacters.kt
+++ src/problems/hackerrank/AlternatingCharacters.kt
@@ -1,5 +1,10 @@
 package problems.hackerrank
 
+/*
+*   HackerRank: Alternating Characters
+*   https://www.hackerrank.com/challenges/alternating-characters/problem
+*/
+
 fun alternatingCharacters(s: String): Int {
     var counter = 0
     for(index in 0 until s.length - 1) {
