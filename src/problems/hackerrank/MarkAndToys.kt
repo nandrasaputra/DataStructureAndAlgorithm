c21574c6f3fa03e009d81c00e0d2f90573f15374
Nandra Saputra <nandra.saputras@gmail.com>
2021-03-10 09:51:27 +0700

    add hackerrank: Mark and Toys Problem and Solution

---
+++ src/problems/hackerrank/MarkAndToys.kt
@@ -0,0 +1,20 @@
+package problems.hackerrank
+
+import kotlin.math.max
+
+// Complete the maximumToys function below.
+fun maximumToys(prices: Array<Int>, k: Int): Int {
+    var maxToys = 0
+    var moneyLeft = k
+    prices.sorted().let { sortedList ->
+        for (index in sortedList.indices) {
+            if (moneyLeft >= sortedList[index]) {
+                moneyLeft -= sortedList[index]
+                maxToys++
+            } else {
+                break
+            }
+        }
+    }
+    return maxToys
+}
