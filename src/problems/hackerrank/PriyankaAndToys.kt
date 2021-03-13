9a6f439522d81209aed3211a71540e55becef606
Nandra Saputra <nandra.saputras@gmail.com>
2021-03-13 10:37:38 +0700

    add hackerrank: Priyanka and Toys Website

--- src/problems/hackerrank/PriyankaAndToys.kt
+++ src/problems/hackerrank/PriyankaAndToys.kt
@@ -1,5 +1,10 @@
 package problems.hackerrank
 
+/*
+*   HackerRank: Priyanka and Toys
+*   https://www.hackerrank.com/challenges/priyanka-and-toys/problem
+*/
+
 fun toys(w: Array<Int>): Int {
     val sortedArray = w.sorted()
     var currentWeight = sortedArray[0]
