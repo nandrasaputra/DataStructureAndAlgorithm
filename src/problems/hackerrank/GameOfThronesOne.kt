6609175bcd6a048c078046a3154de348955cf605
Nandra Saputra <nandra.saputras@gmail.com>
2021-03-19 10:12:58 +0700

    add hackerrank: Game Of Throne - 1 Problem and Solution

---
+++ src/problems/hackerrank/GameOfThronesOne.kt
@@ -0,0 +1,29 @@
+package problems.hackerrank
+
+fun gameOfThrones(s: String): String {
+    val charMap = mutableMapOf<Char, Int>()
+    s.forEach {
+        charMap[it] = charMap.getOrDefault(it, 0) + 1
+    }
+
+    if (s.length % 2 == 0) {
+        for ((key,value) in charMap.entries) {
+            if (value % 2 != 0) {
+                return "NO"
+            }
+        }
+        return "YES"
+    } else {
+        var isEvenCountFound = false
+        for ((key,value) in charMap.entries) {
+            if (value % 2 != 0){
+                if (isEvenCountFound) {
+                    return "NO"
+                } else {
+                    isEvenCountFound = true
+                }
+            }
+        }
+        return "YES"
+    }
+}
