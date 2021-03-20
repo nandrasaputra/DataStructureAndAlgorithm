b3e9f0c1813661a0deb8c0bcc1686f37492f8c9e
Nandra Saputra <nandra.saputras@gmail.com>
2021-03-20 11:11:38 +0700

    add hackerrank tutorial insertion sort 4: Correctness and Loop Invariant

---
+++ src/other/hackerranktutorialchallenges/insertionsort/4CorrectnessAndLoopInvariant.kt
@@ -0,0 +1,20 @@
+package other.hackerranktutorialchallenges.insertionsort
+
+fun insertionSort(A: IntArray) {
+    for (i in 1 until A.size) {
+        val value = A[i]
+        var j = i - 1
+        while (j >= 0 && A[j] > value) {
+            A[j + 1] = A[j]
+            j -= 1
+        }
+        A[j + 1] = value
+    }
+    printArray(A)
+}
+
+fun printArray(ar: IntArray) {
+    for (n in ar) {
+        print("$n ")
+    }
+}
