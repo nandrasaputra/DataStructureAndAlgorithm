c692186a797efd4163d6e9d9b26afc3fd5a96ebf
Nandra Saputra <nandra.saputras@gmail.com>
2021-03-21 08:47:47 +0700

    add hackerrank tutorial insertion sort 5: Running Time of Algorithm

---
+++ src/other/hackerranktutorialchallenges/insertionsort/5RunningTimeOfAlgorithm.kt
@@ -0,0 +1,16 @@
+package other.hackerranktutorialchallenges.insertionsort
+
+fun runningTime(arr: IntArray): Int {
+    var shiftCount = 0
+    for (i in 1 until arr.size) {
+        val value = arr[i]
+        var j = i - 1
+        while (j >= 0 && arr[j] > value) {
+            arr[j + 1] = arr[j]
+            j -= 1
+            shiftCount++
+        }
+        arr[j + 1] = value
+    }
+    return shiftCount
+}
