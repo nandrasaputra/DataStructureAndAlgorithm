a7fc04016bdb595dc236f1fccb2f366c7ad39418
Nandra Saputra <nandra.saputras@gmail.com>
2021-03-26 09:22:18 +0700

    add hackerrank: Two Strings Problem and Solution

---
+++ src/problems/hackerrank/TwoStrings.kt
@@ -0,0 +1,14 @@
+package problems.hackerrank
+
+fun twoStrings(s1: String, s2: String): String {
+    val firstCharMap = mutableMapOf<Char, Int>()
+    s1.forEach {
+        firstCharMap[it] = firstCharMap.getOrDefault(it, 0) + 1
+    }
+    s2.forEach {
+        if (firstCharMap.getOrDefault(it, 0) > 0) {
+            return "YES"
+        }
+    }
+    return "NO"
+}
