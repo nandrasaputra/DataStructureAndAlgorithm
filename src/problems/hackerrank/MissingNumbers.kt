2eb6b0195312ce9082f5d605c1615a35f0eb2af5
Nandra Saputra <nandra.saputras@gmail.com>
2021-03-29 19:28:07 +0700

    add hackerrank: Missing Numbers

---
+++ src/problems/hackerrank/MissingNumbers.kt
@@ -0,0 +1,24 @@
+package problems.hackerrank
+
+fun missingNumbers(arr: Array<Int>, brr: Array<Int>): Array<Int> {
+
+    val frequencyMap = mutableMapOf<Int, Int>()
+
+    brr.forEach {
+        frequencyMap[it] = frequencyMap.getOrDefault(it, 0) + 1
+    }
+
+    arr.forEach {
+        frequencyMap[it] = frequencyMap.getOrDefault(it, 0) - 1
+    }
+
+    val result = mutableListOf<Int>()
+
+    frequencyMap.toSortedMap(compareBy { it }).forEach { (t, u) ->
+        if (u != 0) {
+            result.add(t)
+        }
+    }
+
+    return result.toTypedArray()
+}
