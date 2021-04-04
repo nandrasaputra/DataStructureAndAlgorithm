74a834f73eab5e74144f5a03dc398f13e833117e
Nandra Saputra <nandra.saputras@gmail.com>
2021-04-04 12:10:21 +0700

    add hackerrank: ACM ICPC Team Problem and Solution

---
+++ src/problems/hackerrank/ApcIpcTeam.kt
@@ -0,0 +1,32 @@
+package problems.hackerrank
+
+import java.math.BigInteger
+
+fun acmTeam(topic: Array<String>): Array<Int> {
+    var currentMaxSubject = 0
+    var currentTeamCount = 0
+
+    for (a in 0..topic.size-2) {
+        for (b in (a+1) until topic.size) {
+            val subjectCount = orTwoString(topic[a], topic[b])
+            if (subjectCount > currentMaxSubject) {
+                currentMaxSubject = subjectCount
+                currentTeamCount = 1
+            } else if (subjectCount == currentMaxSubject) {
+                currentTeamCount++
+            }
+        }
+    }
+
+    return arrayOf(
+        currentMaxSubject,
+        currentTeamCount
+    )
+}
+
+private fun orTwoString(firstString : String, secondString: String) : Int {
+    val firstBigInt = BigInteger(firstString, 2)
+    val secondBigInt = BigInteger(secondString, 2)
+    val orResult = firstBigInt or secondBigInt
+    return orResult.bitCount()
+}
