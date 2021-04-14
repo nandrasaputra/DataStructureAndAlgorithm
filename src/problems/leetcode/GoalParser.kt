cd0b01fae53fda3b1c52939a2ed966e82a0f93dd
Nandra Saputra <nandra.saputras@gmail.com>
2021-04-14 09:58:52 +0700

    add leetcode problem and solution: 1678. Goal Parser Interpretation

---
+++ src/problems/leetcode/GoalParser.kt
@@ -0,0 +1,25 @@
+package problems.leetcode
+
+import java.lang.StringBuilder
+
+object GoalParser {
+    fun interpret(command: String): String {
+        val resultStringBuilder = StringBuilder()
+        var currentCharIndex = 0
+        while (currentCharIndex <= command.length-1) {
+            if (command[currentCharIndex] == 'G') {
+                resultStringBuilder.append("G")
+                currentCharIndex += 1
+            } else {
+                if (command[currentCharIndex+1] == 'a') {
+                    resultStringBuilder.append("al")
+                    currentCharIndex += 4
+                } else {
+                    resultStringBuilder.append("o")
+                    currentCharIndex += 2
+                }
+            }
+        }
+        return resultStringBuilder.toString()
+    }
+}
