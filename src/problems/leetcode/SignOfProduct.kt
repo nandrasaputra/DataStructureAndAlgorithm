f4d63645ea34604bea5f9c64c0977f1f6510d561
Nandra Saputra <nandra.saputras@gmail.com>
2021-04-16 09:54:30 +0700

    add leetcode website: 1822. Sign of the Product of an Array

--- src/problems/leetcode/SignOfProduct.kt
+++ src/problems/leetcode/SignOfProduct.kt
@@ -1,5 +1,10 @@
 package problems.leetcode
 
+/*
+*   Leetcode: 1822. Sign of the Product of an Array
+*   https://leetcode.com/problems/sign-of-the-product-of-an-array/
+*/
+
 object SignOfProduct {
     fun arraySign(nums: IntArray): Int {
         var result = 1
