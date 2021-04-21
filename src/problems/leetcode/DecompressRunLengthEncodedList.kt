d119b111c96eec41be60ff9f3a4b81647ba42332
Nandra Saputra <nandra.saputras@gmail.com>
2021-04-22 05:52:07 +0700

    add leetcode website: 1313. Decompress Run-Length Encoded List

--- src/problems/leetcode/DecompressRunLengthEncodedList.kt
+++ src/problems/leetcode/DecompressRunLengthEncodedList.kt
@@ -1,5 +1,10 @@
 package problems.leetcode
 
+/*
+*   Leetcode: 1313. Decompress Run-Length Encoded List
+*   https://leetcode.com/problems/decompress-run-length-encoded-list/
+*/
+
 object DecompressRunLengthEncodedList {
     fun decompressRLElist(nums: IntArray): IntArray {
         val result = mutableListOf<Int>()
