f7a240316244df2f44daa60c34ab4b7aad459d1d
Nandra Saputra <nandra.saputras@gmail.com>
2021-04-25 10:46:44 +0700

    add leetcode web: 1684. Count the Number of Consistent Strings

--- src/problems/leetcode/CountConsistentString.kt
+++ src/problems/leetcode/CountConsistentString.kt
@@ -1,5 +1,10 @@
 package problems.leetcode
 
+/*
+*   Leetcode: 1684. Count the Number of Consistent Strings
+*   https://leetcode.com/problems/count-the-number-of-consistent-strings
+*/
+
 object CountConsistentString {
     fun countConsistentStrings(allowed: String, words: Array<String>): Int {
         var result = 0
