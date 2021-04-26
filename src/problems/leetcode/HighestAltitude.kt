c159a1d92cfa0e2b82417e043c25785eca8dbf20
Nandra Saputra <nandra.saputras@gmail.com>
2021-04-26 10:49:20 +0700

    add leetcode web: 1732. Find the Highest Altitude

--- src/problems/leetcode/HighestAltitude.kt
+++ src/problems/leetcode/HighestAltitude.kt
@@ -1,5 +1,10 @@
 package problems.leetcode
 
+/*
+*   Leetcode: 1732. Find the Highest Altitude
+*   https://leetcode.com/problems/find-the-highest-altitude/
+*/
+
 object HighestAltitude {
     fun largestAltitude(gain: IntArray): Int {
         var highestAltitude = 0
