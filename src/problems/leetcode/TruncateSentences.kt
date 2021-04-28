ad3c68760464b2625c643d054217650ea6720cd4
Nandra Saputra <nandra.saputras@gmail.com>
2021-04-28 09:36:41 +0700

    add leetcode web: 1816. Truncate Sentence

--- src/problems/leetcode/TruncateSentences.kt
+++ src/problems/leetcode/TruncateSentences.kt
@@ -1,5 +1,10 @@
 package problems.leetcode
 
+/*
+*   Leetcode: 1816. Truncate Sentence
+*   https://leetcode.com/problems/truncate-sentence/
+*/
+
 object TruncateSentences {
     fun truncateSentence(s: String, k: Int): String {
         return s.split(" ").take(k).reduce { last, next ->
