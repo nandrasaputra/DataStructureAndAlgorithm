440bf19c00280790003e86bcdc16d2090bcb4f4c
Nandra Saputra <nandra.saputras@gmail.com>
2021-04-30 09:55:31 +0700

    add leetcode web: 1304. Find N Unique Integers Sum up to Zero

--- src/problems/leetcode/SumZero.kt
+++ src/problems/leetcode/SumZero.kt
@@ -1,5 +1,10 @@
 package problems.leetcode
 
+/*
+*   Leetcode: 1304. Find N Unique Integers Sum up to Zero
+*   https://leetcode.com/problems/find-n-unique-integers-sum-up-to-zero/submissions/
+*/
+
 object SumZero {
     fun sumZero(n: Int): IntArray {
         if (n == 0) {
